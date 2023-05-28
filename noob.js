a51deb6b8164258d572ede5bd0c8c13bc431b1d6
rodyrahi <45394091+rodyrahi@users.noreply.github.com>
2023-05-28 18:03:06 +0530

    asd

--- noob.js
+++ noob.js
@@ -137,7 +137,7 @@ app.get("/:name/:parameters", async (req, res) => {
   const modelPath = 'file://public/uploads/models/' + result[0].nickname + '/' + result[0].models;
   const model = await tf.loadLayersModel(modelPath);
 
-  newInput = tf.div(tf.sub(tf.tensor1d([56,3.3]), xsmean), xsstd);
+  const newInput = tf.div(tf.sub(tf.tensor1d([56,3.3]), xsmean), xsstd);
         
   // Predict the price
   const normalizedPrediction = model.predict(newInput.reshape([1, 2]));
